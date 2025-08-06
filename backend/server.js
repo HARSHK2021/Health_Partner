6cf912440b92072b9fd27d790c9db404b0f46461
Dhruv Ratmele <129955117+Somgester@users.noreply.github.com>
2025-08-06 16:56:56 +0530

    deployment_test

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const URL = process.env.FRONTEND_URL;
 initializeSocket(server);
 
 app.use(cors({
-    origin: ["https://healthpartner-fmds.onrender.com", "http://localhost:5173"],
+    origin: ["https://healthpartnerbackend.onrender.com/api/v1", "http://localhost:5173"],
     credentials: true,
 }));
 // app.use(
