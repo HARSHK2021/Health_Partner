593edeba149f7a161f5cebe48e030a4ea45425fd
Somgester <dhruvratmele13@gmail.com>
2026-02-27 16:52:32 +0530

    revert: PatientDetails.jsx styling to previous version

--- frontend/src/pages/PatientDashboard.jsx
+++ frontend/src/pages/PatientDashboard.jsx
@@ -27,9 +27,9 @@ const {user , setUser} = React.useContext(UserDataContext);
   
 
   return (
-    <div className="flex min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-gray-100">
       <Sidebar role="patient" user={user} />
-      <div className="flex-1 ml-64 overflow-auto">
+      <div className="ml-64 transition-all duration-300">
         <div className="bg-gray-100">
           <Outlet /> {/* Render the selected component here */}
         </div>
